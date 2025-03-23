4e7b7c24ab0bad23750037759e9b1faad036afb3
Dmitry <pikalikan77@gmail.com>
2025-03-23 08:10:44 +0300

    Промежуточный коммит перед дебагом спринт 9

---
+++ MyReturn/ProfileViewController.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+final class ProfileViewController:UIViewController {
+    @IBOutlet var vatarImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var loginNameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBOutlet var logoutButton: UIButton!
+    
+    
+    @IBAction func didTapLogoutButton() {
+        
+    }
+    
+}
